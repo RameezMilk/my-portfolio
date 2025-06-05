3de6f0df7f215098a8f550b4bbb757c3e45d5145
Rameez Malik <rmalik.ellahi@gmail.com>
2025-06-05 12:46:45 -0400

    Added a tech stack section, work in progress.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Landing from "./components/Landing";
+import Stack from "./components/Stack";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Landing />
+      <Stack />
       <Hero />
     </>
   );
---
+++ portfolio/src/components/Stack.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Stack.css";
+
+const Stack = () => {
+  return (
+    <section className="stack">
+      <div className="stack-content">
+        <h2 className="stack-title">My Tech Stack</h2>
+        <div className="stack-grid">
+          {/* Add your tech stack items here */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stack; 
